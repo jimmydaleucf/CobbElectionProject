8663d5dbc153f6216de3e46bdb074036723b1846
Jimmy Dale <jimmy.dale@warnermedia.com>
2022-04-22 10:51:43 -0400

    catch up commit

--- notes.md
+++ notes.md
@@ -1,6 +1,6 @@
 Steps to clip a map (county with state legislative districts)
 
 1. import both maps
-2. filter the state county map to the countyu(ies) that you would like ( `filter 'NAME10 =="Cobb"'`)
+2. filter the state county map to the county(ies) that you would like ( `filter 'NAME10 =="Cobb"'`)
 3. then clip with the smaller/filtered file as the input (`-clip Counties_Georgia`)
 4. Then export file that was clipped
--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@
   let svgMarkup;
 
   onMount(() => {
-    fetch("/cobbStateHouse.svg")
+    fetch("/cobbStateHouse2020.svg")
       .then((res) => res.text())
       .then((res) => {
         svgMarkup = res;
@@ -17,6 +17,7 @@
   <h1>Jimmy's Elerction Dershberd</h1>
   {#if svgMarkup}
     <div class="map-container">
+      <h3>2020 State House</h3>
       {@html svgMarkup}
     </div>
   {/if}
@@ -26,8 +27,8 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+    /* max-width: 240px; */
+    /* margin: 0 auto; */
   }
 
   h1 {
@@ -37,13 +38,29 @@
     font-weight: 100;
   }
   .map-container {
+    display: flex;
+    flex-direction: column;
     fill: white;
     border-style: solid;
-  }
+    padding: 40px;
+    border-style: solid;
+    border-color: darkslategray;
+    /* box-shadow: 1px -1px 5px darkslategray; */
+    margin: 10px;
+    min-width: 20%;
 
-  @media (min-width: 640px) {
+    position: absolute;
+  }
+  @import url("https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2&family=Fredoka+One&family=Roboto:ital,wght@0,100;0,400;0,900;1,300;1,700;1,900&display=swap");
+
+  @media (max-width: 480px) {
     main {
       max-width: none;
+    }
+    .map-container {
+      display: flex;
+      min-width: 80%;
+      padding: 5px;
     }
   }
 </style>
